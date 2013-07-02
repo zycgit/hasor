b14dafd640205cc0479640815ec7f0d45704a2fb
zyc <zyc@hasor.net>
2013-07-02 19:49:25 +0800

    剔除正在开发的版本中无关的文件。

---
+++ src/org.platform.core/src/main/java/org/more/util/StringUtils.java
@@ -0,0 +1,386 @@
+/*
+ * Copyright 2008-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.more.util;
+/**
+ * 字符串工具。
+ * @see apache StringBuilder
+ * @version 2010-9-19
+ * @author 赵永春 (zyc@byshell.org)
+ */
+public abstract class StringUtils extends StringConvertUtils {
+    /**
+     * The empty String <code>""</code>.
+     * @since 2.0
+     */
+    public static final String EMPTY           = "";
+    /**
+     * Represents a failed index search.
+     * @since 2.1
+     */
+    public static final int    INDEX_NOT_FOUND = -1;
+    /**
+     * <p>The maximum size to which the padding constant(s) can expand.</p>
+     */
+    private static final int   PAD_LIMIT       = 8192;
+    /**
+     * <p>Checks if a String is empty ("") or null.</p>
+     *
+     * <pre>
+     * StringUtils.isEmpty(null)      = true
+     * StringUtils.isEmpty("")        = true
+     * StringUtils.isEmpty(" ")       = false
+     * StringUtils.isEmpty("bob")     = false
+     * StringUtils.isEmpty("  bob  ") = false
+     * </pre>
+     *
+     * <p>NOTE: This method changed in Lang version 2.0.
+     * It no longer trims the String.
+     * That functionality is available in isBlank().</p>
+     *
+     * @param str  the String to check, may be null
+     * @return <code>true</code> if the String is empty or null
+     */
+    public static boolean isEmpty(String str) {
+        return str == null || str.length() == 0;
+    }
+    /**
+     * <p>Checks if a String is not empty ("") and not null.</p>
+     *
+     * <pre>
+     * StringUtils.isNotEmpty(null)      = false
+     * StringUtils.isNotEmpty("")        = false
+     * StringUtils.isNotEmpty(" ")       = true
+     * StringUtils.isNotEmpty("bob")     = true
+     * StringUtils.isNotEmpty("  bob  ") = true
+     * </pre>
+     *
+     * @param str  the String to check, may be null
+     * @return <code>true</code> if the String is not empty and not null
+     */
+    public static boolean isNotEmpty(String str) {
+        return !StringUtils.isEmpty(str);
+    }
+    /**
+     * <p>Checks if a String is whitespace, empty ("") or null.</p>
+     *
+     * <pre>
+     * StringUtils.isBlank(null)      = true
+     * StringUtils.isBlank("")        = true
+     * StringUtils.isBlank(" ")       = true
+     * StringUtils.isBlank("bob")     = false
+     * StringUtils.isBlank("  bob  ") = false
+     * </pre>
+     *
+     * @param str  the String to check, may be null
+     * @return <code>true</code> if the String is null, empty or whitespace
+     * @since 2.0
+     */
+    public static boolean isBlank(String str) {
+        int strLen;
+        if (str == null || (strLen = str.length()) == 0) {
+            return true;
+        }
+        for (int i = 0; i < strLen; i++) {
+            if ((Character.isWhitespace(str.charAt(i)) == false)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    /**
+     * <p>Checks if a String is not empty (""), not null and not whitespace only.</p>
+     *
+     * <pre>
+     * StringUtils.isNotBlank(null)      = false
+     * StringUtils.isNotBlank("")        = false
+     * StringUtils.isNotBlank(" ")       = false
+     * StringUtils.isNotBlank("bob")     = true
+     * StringUtils.isNotBlank("  bob  ") = true
+     * </pre>
+     *
+     * @param str  the String to check, may be null
+     * @return <code>true</code> if the String is
+     *  not empty and not null and not whitespace
+     * @since 2.0
+     */
+    public static boolean isNotBlank(String str) {
+        return !StringUtils.isBlank(str);
+    }
+    /**
+     * <p>Replaces all occurrences of a String within another String.</p>
+     *
+     * <p>A <code>null</code> reference passed to this method is a no-op.</p>
+     *
+     * <pre>
+     * StringUtils.replace(null, *, *)        = null
+     * StringUtils.replace("", *, *)          = ""
+     * StringUtils.replace("any", null, *)    = "any"
+     * StringUtils.replace("any", *, null)    = "any"
+     * StringUtils.replace("any", "", *)      = "any"
+     * StringUtils.replace("aba", "a", null)  = "aba"
+     * StringUtils.replace("aba", "a", "")    = "b"
+     * StringUtils.replace("aba", "a", "z")   = "zbz"
+     * </pre>
+     *
+     * @see #replace(String text, String searchString, String replacement, int max)
+     * @param text  text to search and replace in, may be null
+     * @param searchString  the String to search for, may be null
+     * @param replacement  the String to replace it with, may be null
+     * @return the text with any replacements processed,
+     *  <code>null</code> if null String input
+     */
+    public static String replace(String text, String searchString, String replacement) {
+        return replace(text, searchString, replacement, -1);
+    }
+    /**
+     * <p>Replaces a String with another String inside a larger String,
+     * for the first <code>max</code> values of the search String.</p>
+     *
+     * <p>A <code>null</code> reference passed to this method is a no-op.</p>
+     *
+     * <pre>
+     * StringUtils.replace(null, *, *, *)         = null
+     * StringUtils.replace("", *, *, *)           = ""
+     * StringUtils.replace("any", null, *, *)     = "any"
+     * StringUtils.replace("any", *, null, *)     = "any"
+     * StringUtils.replace("any", "", *, *)       = "any"
+     * StringUtils.replace("any", *, *, 0)        = "any"
+     * StringUtils.replace("abaa", "a", null, -1) = "abaa"
+     * StringUtils.replace("abaa", "a", "", -1)   = "b"
+     * StringUtils.replace("abaa", "a", "z", 0)   = "abaa"
+     * StringUtils.replace("abaa", "a", "z", 1)   = "zbaa"
+     * StringUtils.replace("abaa", "a", "z", 2)   = "zbza"
+     * StringUtils.replace("abaa", "a", "z", -1)  = "zbzz"
+     * </pre>
+     *
+     * @param text  text to search and replace in, may be null
+     * @param searchString  the String to search for, may be null
+     * @param replacement  the String to replace it with, may be null
+     * @param max  maximum number of values to replace, or <code>-1</code> if no maximum
+     * @return the text with any replacements processed,
+     *  <code>null</code> if null String input
+     */
+    public static String replace(String text, String searchString, String replacement, int max) {
+        if (isEmpty(text) || isEmpty(searchString) || replacement == null || max == 0) {
+            return text;
+        }
+        int start = 0;
+        int end = text.indexOf(searchString, start);
+        if (end == INDEX_NOT_FOUND) {
+            return text;
+        }
+        int replLength = searchString.length();
+        int increase = replacement.length() - replLength;
+        increase = (increase < 0 ? 0 : increase);
+        increase *= (max < 0 ? 16 : (max > 64 ? 64 : max));
+        StringBuilder buf = new StringBuilder(text.length() + increase);
+        while (end != INDEX_NOT_FOUND) {
+            buf.append(text.substring(start, end)).append(replacement);
+            start = end + replLength;
+            if (--max == 0) {
+                break;
+            }
+            end = text.indexOf(searchString, start);
+        }
+        buf.append(text.substring(start));
+        return buf.toString();
+    }
+    /**转换首字母大写。*/
+    public static String toUpperCase(String value) {
+        StringBuffer sb = new StringBuffer(value);
+        char firstChar = sb.charAt(0);
+        sb.delete(0, 1);
+        sb.insert(0, (char) ((firstChar >= 97) ? firstChar - 32 : firstChar));
+        return sb.toString();
+    }
+    /**转换首字母小写。*/
+    public static String toLowerCase(String value) {
+        StringBuffer sb = new StringBuffer(value);
+        char firstChar = sb.charAt(0);
+        sb.delete(0, 1);
+        sb.insert(0, (char) ((firstChar <= 90) ? firstChar + 32 : firstChar));
+        return sb.toString();
+    }
+    /**将通配符转换成正则表达式。*/
+    public static String wildToRegex(String wild) {
+        if (wild == null)
+            throw new NullPointerException("wild param is null");
+        StringBuffer result = new StringBuffer("");
+        char metachar[] = { '$', '^', '[', ']', '(', ')', '{', '|', '+', '.', '\\' };
+        for (int i = 0; i < wild.length(); i++) {
+            char ch = wild.charAt(i);
+            for (int j = 0; j < metachar.length; j++)
+                if (ch == metachar[j])
+                    result.append("\\");
+            if (ch == '*')
+                result.append(".*");
+            else if (ch == '?')
+                result.append(".");
+            else
+                result.append(ch);
+        }
+        result.append("$");
+        return result.toString();
+    }
+    /**将字符串转换成成正则表达式。*/
+    public static String stringToRegex(String wild) {
+        if (wild == null)
+            throw new NullPointerException("wild param is null");
+        StringBuffer result = new StringBuffer("");
+        char metachar[] = { '$', '^', '[', ']', '(', ')', '{', '|', '+', '.', '\\' };
+        for (int i = 0; i < wild.length(); i++) {
+            char ch = wild.charAt(i);
+            for (int j = 0; j < metachar.length; j++)
+                if (ch == metachar[j])
+                    result.append("\\");
+            result.append(ch);
+        }
+        return result.toString();
+    }
+    /**使用通配符匹配字符串。*/
+    public static boolean matchWild(String pattern, String str) {
+        if (str == null)
+            return false;
+        return str.matches(wildToRegex(pattern));
+    }
+    /** 字符串转换成十六进制字符串 */
+    public static String str2HexStr(String str) {
+        char[] chars = "0123456789ABCDEF".toCharArray();
+        StringBuilder sb = new StringBuilder("");
+        byte[] bs = str.getBytes();
+        int bit;
+        for (int i = 0; i < bs.length; i++) {
+            bit = (bs[i] & 0x0f0) >> 4;
+            sb.append(chars[bit]);
+            bit = bs[i] & 0x0f;
+            sb.append(chars[bit]);
+        }
+        return sb.toString();
+    }
+    /** 十六进制转换字符串 */
+    public static String hexStr2Str(String hexStr) {
+        String str = "0123456789ABCDEF";
+        char[] hexs = hexStr.toCharArray();
+        byte[] bytes = new byte[hexStr.length() / 2];
+        int n;
+        for (int i = 0; i < bytes.length; i++) {
+            n = str.indexOf(hexs[2 * i]) * 16;
+            n += str.indexOf(hexs[2 * i + 1]);
+            bytes[i] = (byte) (n & 0xff);
+        }
+        return new String(bytes);
+    }
+    /** bytes转换成十六进制字符串 */
+    public static String byte2HexStr(byte[] b) {
+        String hs = "";
+        String stmp = "";
+        for (int n = 0; n < b.length; n++) {
+            stmp = (Integer.toHexString(b[n] & 0XFF));
+            if (stmp.length() == 1)
+                hs = hs + "0" + stmp;
+            else
+                hs = hs + stmp;
+            //if (n<b.length-1) hs=hs+":";
+        }
+        return hs.toUpperCase();
+    }
+    private static byte uniteBytes(String src0, String src1) {
+        byte b0 = Byte.decode("0x" + src0).byteValue();
+        b0 = (byte) (b0 << 4);
+        byte b1 = Byte.decode("0x" + src1).byteValue();
+        byte ret = (byte) (b0 | b1);
+        return ret;
+    }
+    /** bytes转换成十六进制字符串 */
+    public static byte[] hexStr2Bytes(String src) {
+        int m = 0, n = 0;
+        int l = src.length() / 2;
+        //System.out.println(l);
+        byte[] ret = new byte[l];
+        for (int i = 0; i < l; i++) {
+            m = i * 2 + 1;
+            n = m + 1;
+            ret[i] = uniteBytes(src.substring(i * 2, m), src.substring(m, n));
+        }
+        return ret;
+    }
+    /** String的字符串转换成unicode的String */
+    public static String stringToUnicode(String strText) throws Exception {
+        char c;
+        String strRet = "";
+        int intAsc;
+        String strHex;
+        for (int i = 0; i < strText.length(); i++) {
+            c = strText.charAt(i);
+            intAsc = (int) c;
+            strHex = Integer.toHexString(intAsc);
+            if (intAsc > 128) {
+                strRet += "\\u" + strHex;
+            } else {
+                // 低位在前面补00
+                strRet += "\\u00" + strHex;
+            }
+        }
+        return strRet;
+    }
+    /** unicode的String转换成String的字符串 */
+    public static String unicodeToString(String hex) {
+        int t = hex.length() / 6;
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < t; i++) {
+            String s = hex.substring(i * 6, (i + 1) * 6);
+            // 高位需要补上00再转
+            String s1 = s.substring(2, 4) + "00";
+            // 低位直接转
+            String s2 = s.substring(4);
+            // 将16进制的string转为int
+            int n = Integer.valueOf(s1, 16) + Integer.valueOf(s2, 16);
+            // 将int转换为字符
+            char[] chars = Character.toChars(n);
+            str.append(new String(chars));
+        }
+        return str.toString();
+    }
+    /**忽略大小写敏感匹配字符串*/
+    public static boolean eqUnCaseSensitive(String str1, String str2) {
+        if (str1 == null && str2 == null)
+            return true;
+        if ((str1 == null && str2 != null) || (str1 != null && str2 == null))
+            return false;
+        //
+        String s1 = str1.toLowerCase();
+        String s2 = str2.toLowerCase();
+        return s1.equals(s2);
+    }
+    /**
+     * 执行字符串匹配。
+     * <pre>
+     * StringUtils.eqUnCaseBlankSensitive(null,"")         = true
+     * StringUtils.eqUnCaseBlankSensitive("","")           = true
+     * StringUtils.eqUnCaseBlankSensitive(" ","")          = true
+     * StringUtils.eqUnCaseBlankSensitive(" ",null)        = true
+     * StringUtils.eqUnCaseBlankSensitive("bob","")        = false
+     * StringUtils.eqUnCaseBlankSensitive("  bob  ","bob") = true
+     * </pre> 
+     */
+    public static boolean eqUnCaseBlankSensitive(String str1, String str2) {
+        if (isBlank(str1) && isBlank(str2))
+            return true;
+        if (!isBlank(str1) && !isBlank(str2))
+            return eqUnCaseSensitive(str1.trim(), str2.trim());
+        return false;
+    }
+};
