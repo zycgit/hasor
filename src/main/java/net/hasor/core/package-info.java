0cae7fd99ea88fc07b79ed3a3c5741f5d9656bc0
zyc <zyc@hasor.net>
2015-09-10 11:55:37 +0800

    1.项目目录结构变更，Hasor源码库更加单一。

---
+++ src/main/java/net/hasor/core/package-info.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2008-2009 the original 赵永春(zyc@hasor.net).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * Hasor的核心软件包，几乎所有Hasor扩展模块都会依赖到它。包含工具和 Hasor 两个部分，它是整个Hasor蓝图的基础。
+ * 该软件包提供了：配置文件解析、事件、容器、IoC/Aop等核心功能。
+ */
+package net.hasor.core;
