5c36804c7d01bd162883887c46b7fdbb50a68681
zyc <zyc@hasor.net>
2026-06-19 23:30:26 +0800

    feat(hasor-boot): 新增内嵌 Web 容器适配模块
    - 新增 hasor-boot-tomcat、hasor-boot-jetty、hasor-boot-undertow 三个容器模块
    - 分别封装 Tomcat、Jetty、Undertow 的 WebServer 实现，统一接入 Hasor Web 的 RuntimeListener 和 RuntimeFilter
    - 通过 WebServerProvider 暴露 tomcat、jetty、undertow 三种 Java SPI 容器提供者
    - 支持 host、port、contextPath、documentRoot、filter 参数和 ServletContext 属性传递
    - 各容器模块独立声明自身运行期依赖版本，避免父级统一管理容器版本
    - 增加三个容器的启动停止测试，验证 SPI 创建、随机端口绑定和生命周期状态

---
+++ hasor-boot/hasor-boot-jetty/src/main/java/net/hasor/boot/web/jetty/JettyWebServerProvider.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2008-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.hasor.boot.web.jetty;
+import net.hasor.web.http.WebServer;
+import net.hasor.web.http.WebServerConfig;
+import net.hasor.web.http.WebServerProvider;
+/**
+ * Java SPI provider for embedded Jetty.
+ * @author 赵永春 (zyc@hasor.net)
+ * @version : 2026-06-18
+ */
+public class JettyWebServerProvider implements WebServerProvider {
+    @Override
+    public String name() {
+        return "jetty";
+    }
+
+    @Override
+    public WebServer create(WebServerConfig config) {
+        return new JettyWebServer(config);
+    }
+}
---
+++ hasor-boot/hasor-boot-tomcat/src/test/java/net/hasor/boot/web/tomcat/TomcatWebServerTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2008-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.hasor.boot.web.tomcat;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import net.hasor.web.WebApiBinder;
+import net.hasor.web.WebModule;
+import net.hasor.web.http.WebServer;
+import net.hasor.web.http.WebServerConfig;
+import net.hasor.web.http.WebServers;
+
+public class TomcatWebServerTest {
+    @Test
+    public void startStop() throws Exception {
+        WebServer server = WebServers.create(WebServerConfig.of(StartModule.class).server("tomcat").port(0));
+        assertTrue(server instanceof TomcatWebServer);
+        try {
+            server.start();
+            assertTrue(server.isStart());
+            assertTrue(server.getPort() > 0);
+        } finally {
+            server.stop();
+        }
+        assertFalse(server.isStart());
+    }
+
+    public static class StartModule implements WebModule {
+        @Override
+        public void loadModule(WebApiBinder apiBinder) {
+            //
+        }
+    }
+}
