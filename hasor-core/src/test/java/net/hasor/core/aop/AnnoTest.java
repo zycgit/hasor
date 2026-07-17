c7a3be84bac0d8c4f71b8cdc6d054652bfa63abc
zyc <zyc@hasor.net>
2026-07-18 00:49:38 +0800

    1. hasor-core 新增支持遇到循环依赖时打印修女换依赖连。

--- hasor-core/src/test/java/net/hasor/core/aop/AnnoTest.java
+++ hasor-core/src/test/java/net/hasor/core/aop/AnnoTest.java
@@ -14,13 +14,9 @@
  * limitations under the License.
  */
 package net.hasor.core.aop;
-import net.hasor.cobble.dynamic.DynamicClass;
 import net.hasor.cobble.dynamic.Matchers;
 import net.hasor.core.AppContext;
 import net.hasor.core.Hasor;
-import net.hasor.test.core.aop.anno.AopBean;
-import net.hasor.test.core.aop.anno.ClassAnnoInterceptor;
-import net.hasor.test.core.aop.anno.MethodAnnoInterceptor;
 import net.hasor.test.core.aop.custom.ClassMyAopBean;
 import net.hasor.test.core.aop.custom.MethodMyAopBean;
 import net.hasor.test.core.aop.custom.MyAop;
@@ -28,79 +24,9 @@ import net.hasor.test.core.aop.custom.MyAopInterceptor;
 import org.junit.Test;
 
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.function.Predicate;
 
 public class AnnoTest {
-    @Test
-    public void sampleAopTest1() {
-        try (AppContext appContext = Hasor.create().build()) {
-            AopBean instance = appContext.getInstance(AopBean.class);
-            assert instance instanceof DynamicClass;
-        }
-    }
-
-    @Test
-    public void aopTest1() {
-        ClassAnnoInterceptor classInterceptor = new ClassAnnoInterceptor();
-        MethodAnnoInterceptor methodInterceptor = new MethodAnnoInterceptor();
-        AppContext appContext = Hasor.create().build(apiBinder -> {
-            apiBinder.bindType(ClassAnnoInterceptor.class, classInterceptor);
-            apiBinder.bindType(MethodAnnoInterceptor.class, methodInterceptor);
-        });
-        //
-        AopBean instance = appContext.getInstance(AopBean.class);
-        assert instance instanceof DynamicClass;
-        //
-        instance.doInit(new ArrayList<>());
-        assert methodInterceptor.getCallInfo().get("doInit") == null;
-        assert classInterceptor.getCallInfo().get("doInit").size() == 2;
-        //
-        List<?> objects = instance.checkBaseType(true, (byte) 1, (short) 2, 3, 4, 5.0f, 6.0d, 'c');
-        assert objects.size() == 8;
-        assert (Boolean) objects.get(0);
-        assert (Byte) objects.get(1) == 1;
-        assert (Short) objects.get(2) == 2;
-        assert (Integer) objects.get(3) == 3;
-        assert (Long) objects.get(4) == 4;
-        assert (Float) objects.get(5) == 5.0f;
-        assert (Double) objects.get(6) == 6.0d;
-        assert (Character) objects.get(7) == 'c';
-        //
-        assert instance.aBooleanValue(true) &&//
-                classInterceptor.getCallInfo().get("aBooleanValue").size() == 2 &&//
-                methodInterceptor.getCallInfo().get("aBooleanValue").size() == 2;
-        //
-        assert instance.aByteValue((byte) 1) == 1 &&//
-                classInterceptor.getCallInfo().get("aByteValue").size() == 2 &&//
-                methodInterceptor.getCallInfo().get("aByteValue").size() == 2;
-        //
-        assert instance.aShort((short) 2) == 2 &&//
-                classInterceptor.getCallInfo().get("aShort").size() == 2 &&//
-                methodInterceptor.getCallInfo().get("aShort").size() == 2;
-        //
-        assert instance.aIntValue(3) == 3 &&//
-                classInterceptor.getCallInfo().get("aIntValue").size() == 2 &&//
-                methodInterceptor.getCallInfo().get("aIntValue").size() == 2;
-        //
-        assert instance.aLongValue(4) == 4 &&//
-                classInterceptor.getCallInfo().get("aLongValue").size() == 2 &&//
-                methodInterceptor.getCallInfo().get("aLongValue").size() == 2;
-        //
-        assert instance.aFloatValue(5.5f) == 5.5f &&//
-                classInterceptor.getCallInfo().get("aFloatValue").size() == 2 &&//
-                methodInterceptor.getCallInfo().get("aFloatValue").size() == 2;
-        //
-        assert instance.aDoubleValue(6.0d) == 6.0d &&//
-                classInterceptor.getCallInfo().get("aLongValue").size() == 2 &&//
-                methodInterceptor.getCallInfo().get("aLongValue").size() == 2;
-        //
-        assert instance.aCharValue('a') == 'a' &&//
-                classInterceptor.getCallInfo().get("aCharValue").size() == 2 &&//
-                methodInterceptor.getCallInfo().get("aCharValue").size() == 2;
-    }
-
     @Test
     public void aopTest2() {
         AppContext appContext = Hasor.create().build(apiBinder -> {
